03093deb42faecba3c2e5c890addf79f53385eb8
Jone <jone@ploing.de>
2026-01-11 08:26:46 +0100

    reformatting

--- button-bar.js
+++ button-bar.js
@@ -1,111 +1,119 @@
 async function fetchGameLibrary() {
-	const response = await fetch("games.json", {
-		method: "GET",
-		cache: "reload"
-	});
-	if (!response.ok) {
-		throw new Error(response.statusText);
-	}
-	return await response.json();
+  const response = await fetch("games.json", {
+    method: "GET",
+    cache: "reload",
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
 }
 
-window.addEventListener("load", _ => {
-	const addGameButton = document.getElementById("add-game-button");
-	const addGameDialog = document.getElementById("add-game-dialog");
-	addGameButton.addEventListener("click", () => {
-		const gameLibraryElement = addGameDialog.querySelector("#game-library");
-		gameLibraryElement.innerHTML = "";
-		fetchGameLibrary().then(gameLibrary => {
-			gameLibrary.forEach(game => {
-				const renderedTemplate = gameTemplate.content.cloneNode(true);
-				const gameElement = renderedTemplate.querySelector(".game");
-				gameElement.classList.add("available-game");
-				gameElement.dataset.gameId = game.id;
-				gameElement.style.backgroundImage = `url(${game.cover})`;
-				const gameNameElement = gameElement.querySelector(".game-name");
-				gameNameElement.innerText = game.name;
-				gameElement.addEventListener("click", _ => {
-					const gameNameInput = addGameDialog.querySelector("#game-name-input");
-					gameNameInput.value = game.name;
-					const gameIDInput = addGameDialog.querySelector("#game-id-input");
-					gameIDInput.value = game.id;
-					const gameImageInput = addGameDialog.querySelector("#game-image-input");
-					fetch(game.cover).then(response => {
-						if (response.ok) {
-							return response.blob();
-						} else {
-							throw new Error(response.statusText);
-						}
-					}).then(blob => {
-						const dataTransfer = new DataTransfer();
-						const file = new File([blob], game.cover.split("/").pop().split("?").shift(), {type: blob.type})
-						dataTransfer.items.add(file);
-						gameImageInput.files = dataTransfer.files;
-					});
-				});
-				gameLibraryElement.appendChild(gameElement);
-			});
-			addGameDialog.showModal();
-		});
-	});
-	const addGameForm = document.getElementById("add-game-form");
-	addGameForm.addEventListener("submit", _ => {
-		const name = document.getElementById("game-name-input").value;
-		const id = document.getElementById("game-id-input").value;
-		const coverFile = document.getElementById("game-image-input").files[0];
-		const canvas = document.getElementById("downscale-canvas");
-		const ctx = canvas.getContext("2d");
-		const img = new Image();
-		img.addEventListener("load", _ => {
-			const viewportWidth = window.innerWidth;
-			const viewportHeight = window.innerHeight;
-			const aspectRatio = img.width / img.height;
-			let desiredWidth, desiredHeight;
-			if (viewportWidth / viewportHeight > aspectRatio) {
-				desiredHeight = viewportHeight;
-				desiredWidth = desiredHeight * aspectRatio;
-			} else {
-				desiredWidth = viewportWidth;
-				desiredHeight = desiredWidth / aspectRatio;
-			}
-			canvas.width = desiredWidth;
-			canvas.height = desiredHeight;
-			const gameBGColor = getComputedStyle(canvas).getPropertyValue("--game-bg-color");
-			ctx.fillStyle = `${gameBGColor}`;
-			ctx.fillRect(0, 0, canvas.width, canvas.height);
-			ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-			const cover = canvas.toDataURL("image/jpeg");
-			const game = {name, id, cover};
-			const currentGames = fetchGames();
-			const games = currentGames.filter(game => game.id !== id)
-			games.unshift(game);
-			localStorage.games = JSON.stringify(games);
-			renderGames();
-		});
-		const reader = new FileReader();
-		reader.addEventListener("load", e => {
-			img.src = e.target.result;
-		});
-		reader.readAsDataURL(coverFile);
-	});
-	const resetGamesButton = document.getElementById("reset-games-button");
-	resetGamesButton.addEventListener("click", _ => {
-		if (!confirm("Are you sure to reset all games?")) {
-			return;
-		}
-		localStorage.games = JSON.stringify([]);
-		renderGames();
-		addGameDialog.close();
-	});
-	const removeGameButton = document.getElementById("remove-game-button");
-	removeGameButton.addEventListener("click", _ => {
-		if (!confirm("Are you sure to remove the current game?")) {
-			return;
-		}
-		const oldGames = fetchGames();
-		const currentGame = getCurrentGame();
-		const games = oldGames.filter(game => game.id !== currentGame);
-		localStorage.games = JSON.stringify(games);
-		renderGames();
-	});
+window.addEventListener("load", (_) => {
+  const addGameButton = document.getElementById("add-game-button");
+  const addGameDialog = document.getElementById("add-game-dialog");
+  addGameButton.addEventListener("click", () => {
+    const gameLibraryElement = addGameDialog.querySelector("#game-library");
+    gameLibraryElement.innerHTML = "";
+    fetchGameLibrary().then((gameLibrary) => {
+      gameLibrary.forEach((game) => {
+        const renderedTemplate = gameTemplate.content.cloneNode(true);
+        const gameElement = renderedTemplate.querySelector(".game");
+        gameElement.classList.add("available-game");
+        gameElement.dataset.gameId = game.id;
+        gameElement.style.backgroundImage = `url(${game.cover})`;
+        const gameNameElement = gameElement.querySelector(".game-name");
+        gameNameElement.innerText = game.name;
+        gameElement.addEventListener("click", (_) => {
+          const gameNameInput = addGameDialog.querySelector("#game-name-input");
+          gameNameInput.value = game.name;
+          const gameIDInput = addGameDialog.querySelector("#game-id-input");
+          gameIDInput.value = game.id;
+          const gameImageInput =
+            addGameDialog.querySelector("#game-image-input");
+          fetch(game.cover)
+            .then((response) => {
+              if (response.ok) {
+                return response.blob();
+              } else {
+                throw new Error(response.statusText);
+              }
+            })
+            .then((blob) => {
+              const dataTransfer = new DataTransfer();
+              const file = new File(
+                [blob],
+                game.cover.split("/").pop().split("?").shift(),
+                { type: blob.type },
+              );
+              dataTransfer.items.add(file);
+              gameImageInput.files = dataTransfer.files;
+            });
+        });
+        gameLibraryElement.appendChild(gameElement);
+      });
+      addGameDialog.showModal();
+    });
+  });
+  const addGameForm = document.getElementById("add-game-form");
+  addGameForm.addEventListener("submit", (_) => {
+    const name = document.getElementById("game-name-input").value;
+    const id = document.getElementById("game-id-input").value;
+    const coverFile = document.getElementById("game-image-input").files[0];
+    const canvas = document.getElementById("downscale-canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.addEventListener("load", (_) => {
+      const viewportWidth = window.innerWidth;
+      const viewportHeight = window.innerHeight;
+      const aspectRatio = img.width / img.height;
+      let desiredWidth, desiredHeight;
+      if (viewportWidth / viewportHeight > aspectRatio) {
+        desiredHeight = viewportHeight;
+        desiredWidth = desiredHeight * aspectRatio;
+      } else {
+        desiredWidth = viewportWidth;
+        desiredHeight = desiredWidth / aspectRatio;
+      }
+      canvas.width = desiredWidth;
+      canvas.height = desiredHeight;
+      const gameBGColor =
+        getComputedStyle(canvas).getPropertyValue("--game-bg-color");
+      ctx.fillStyle = `${gameBGColor}`;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      const cover = canvas.toDataURL("image/jpeg");
+      const game = { name, id, cover };
+      const currentGames = fetchGames();
+      const games = currentGames.filter((game) => game.id !== id);
+      games.unshift(game);
+      localStorage.games = JSON.stringify(games);
+      renderGames();
+    });
+    const reader = new FileReader();
+    reader.addEventListener("load", (e) => {
+      img.src = e.target.result;
+    });
+    reader.readAsDataURL(coverFile);
+  });
+  const resetGamesButton = document.getElementById("reset-games-button");
+  resetGamesButton.addEventListener("click", (_) => {
+    if (!confirm("Are you sure to reset all games?")) {
+      return;
+    }
+    localStorage.games = JSON.stringify([]);
+    renderGames();
+    addGameDialog.close();
+  });
+  const removeGameButton = document.getElementById("remove-game-button");
+  removeGameButton.addEventListener("click", (_) => {
+    if (!confirm("Are you sure to remove the current game?")) {
+      return;
+    }
+    const oldGames = fetchGames();
+    const currentGame = getCurrentGame();
+    const games = oldGames.filter((game) => game.id !== currentGame);
+    localStorage.games = JSON.stringify(games);
+    renderGames();
+  });
 });
--- games.js
+++ games.js
@@ -2,153 +2,159 @@ let gamesElement = null;
 let gameTemplate = null;
 
 function games_checkForVariables() {
-	const errorMsgNotFound = "VARIABLE not found. Is the page fully loaded?";
-	const checkForNull = (variable, name) => {
-		if (variable === null) {
-			throw new Error(errorMsgNotFound.replace("VARIABLE", name));
-		}
-	}
-	checkForNull(gamesElement, "Games element");
-	checkForNull(gameTemplate, "Game template");
+  const errorMsgNotFound = "VARIABLE not found. Is the page fully loaded?";
+  const checkForNull = (variable, name) => {
+    if (variable === null) {
+      throw new Error(errorMsgNotFound.replace("VARIABLE", name));
+    }
+  };
+  checkForNull(gamesElement, "Games element");
+  checkForNull(gameTemplate, "Game template");
 }
 
 function switchGame(gameID) {
-	games_checkForVariables();
-	const gameElement = gamesElement.querySelector(`[data-game-id="${gameID.replace(/(["\\])/g, '\\$1')}"]`);
-	if (gameElement === null) {
-		throw new Error("Game not found");
-	}
-	["prev-game", "current-game", "next-game"].forEach(id => {
-		const matchingElementForID = gamesElement.querySelector(`#${id}`);
-		if (matchingElementForID) {
-			matchingElementForID.removeAttribute("id");
-		}
-	});
-	gameElement.id = "current-game";
-	let prevGame = gameElement.previousSibling;
-	while (prevGame && prevGame.nodeType !== prevGame.ELEMENT_NODE) {
-		prevGame = prevGame.previousSibling;
-	}
-	if (prevGame) {
-		prevGame.id = "prev-game";
-	}
-	let nextGame = gameElement.nextSibling;
-	while (nextGame && nextGame.nodeType !== nextGame.ELEMENT_NODE) {
-		nextGame = nextGame.nextSibling;
-	}
-	if (nextGame) {
-		nextGame.id = "next-game";
-	}
+  games_checkForVariables();
+  const gameElement = gamesElement.querySelector(
+    `[data-game-id="${gameID.replace(/(["\\])/g, "\\$1")}"]`,
+  );
+  if (gameElement === null) {
+    throw new Error("Game not found");
+  }
+  ["prev-game", "current-game", "next-game"].forEach((id) => {
+    const matchingElementForID = gamesElement.querySelector(`#${id}`);
+    if (matchingElementForID) {
+      matchingElementForID.removeAttribute("id");
+    }
+  });
+  gameElement.id = "current-game";
+  let prevGame = gameElement.previousSibling;
+  while (prevGame && prevGame.nodeType !== prevGame.ELEMENT_NODE) {
+    prevGame = prevGame.previousSibling;
+  }
+  if (prevGame) {
+    prevGame.id = "prev-game";
+  }
+  let nextGame = gameElement.nextSibling;
+  while (nextGame && nextGame.nodeType !== nextGame.ELEMENT_NODE) {
+    nextGame = nextGame.nextSibling;
+  }
+  if (nextGame) {
+    nextGame.id = "next-game";
+  }
 }
 
 function fetchGames() {
-	let games = localStorage.getItem("games") || "[]";
-	games = JSON.parse(games);
-	return games;
+  let games = localStorage.getItem("games") || "[]";
+  games = JSON.parse(games);
+  return games;
 }
 
 function renderGames(template = gameTemplate, target = gamesElement) {
-	games_checkForVariables();
-	const games = fetchGames();
-	target.innerHTML = "";
-	games.forEach(game => {
-		game.cover = game.cover || "about:blank";
-		const renderedTemplate = template.content.cloneNode(true);
-		const gameElement = renderedTemplate.querySelector(".game");
-		gameElement.dataset.gameId = game.id;
-		gameElement.style.backgroundImage = `url(${game.cover})`;
-		const gameName = renderedTemplate.querySelector(".game-name");
-		gameName.innerText = game.name;
-		target.appendChild(renderedTemplate);
-	});
-	const firstGame = gamesElement.querySelector(".game");
-	if (firstGame) {
-		switchGame(firstGame.dataset.gameId); // Switch to 1st game
-	}
+  games_checkForVariables();
+  const games = fetchGames();
+  target.innerHTML = "";
+  games.forEach((game) => {
+    game.cover = game.cover || "about:blank";
+    const renderedTemplate = template.content.cloneNode(true);
+    const gameElement = renderedTemplate.querySelector(".game");
+    gameElement.dataset.gameId = game.id;
+    gameElement.style.backgroundImage = `url(${game.cover})`;
+    const gameName = renderedTemplate.querySelector(".game-name");
+    gameName.innerText = game.name;
+    target.appendChild(renderedTemplate);
+  });
+  const firstGame = gamesElement.querySelector(".game");
+  if (firstGame) {
+    switchGame(firstGame.dataset.gameId); // Switch to 1st game
+  }
 }
 
 function startGame(gameID) {
-	window.location.href = `gamescreen://start/${gameID}`;
+  window.location.href = `gamescreen://start/${gameID}`;
 }
 
 function getCurrentGameElement() {
-	const currentGameElement = gamesElement.querySelector("#current-game");
-	if (currentGameElement === null) {
-		return null;
-	} else {
-		return currentGameElement;
-	}
+  const currentGameElement = gamesElement.querySelector("#current-game");
+  if (currentGameElement === null) {
+    return null;
+  } else {
+    return currentGameElement;
+  }
 }
 
 function getCurrentGame() {
-	return getCurrentGameElement().dataset.gameId;
+  return getCurrentGameElement().dataset.gameId;
 }
 
 function prevGame() {
-	games_checkForVariables();
-	let gameElement = getCurrentGameElement();
-	let prevGame = gameElement.previousSibling;
-	while (prevGame && prevGame.nodeType !== prevGame.ELEMENT_NODE) {
-		prevGame = prevGame.previousSibling;
-	}
-	if (prevGame) {
-		switchGame(prevGame.dataset.gameId);
-	}
+  games_checkForVariables();
+  let gameElement = getCurrentGameElement();
+  let prevGame = gameElement.previousSibling;
+  while (prevGame && prevGame.nodeType !== prevGame.ELEMENT_NODE) {
+    prevGame = prevGame.previousSibling;
+  }
+  if (prevGame) {
+    switchGame(prevGame.dataset.gameId);
+  }
 }
 
 function nextGame() {
-	games_checkForVariables();
-	let gameElement = getCurrentGameElement();
-	let nextGame = gameElement.nextSibling;
-	while (nextGame && nextGame.nodeType !== nextGame.ELEMENT_NODE) {
-		nextGame = nextGame.nextSibling;
-	}
-	if (nextGame) {
-		switchGame(nextGame.dataset.gameId);
-	}
+  games_checkForVariables();
+  let gameElement = getCurrentGameElement();
+  let nextGame = gameElement.nextSibling;
+  while (nextGame && nextGame.nodeType !== nextGame.ELEMENT_NODE) {
+    nextGame = nextGame.nextSibling;
+  }
+  if (nextGame) {
+    switchGame(nextGame.dataset.gameId);
+  }
 }
 
 window.addEventListener("load", () => {
-	gameTemplate = document.querySelector("#game-template");
-	gamesElement = document.querySelector("#games");
-	renderGames();
-	gamesElement.addEventListener("click", (event) => {
-		let gameElement = event.target;
-		while (gameElement && !gameElement.classList.contains("game")) {
-			gameElement = gameElement.parentElement;
-		}
-		if (gameElement && gameElement.dataset.gameId) {
-			const gameID = gameElement.dataset.gameId;
-			if (gameElement.id === "current-game") {
-				startGame(gameID);
-			} else {
-				switchGame(gameID)
-			}
-		} else {
-			console.warn("No game ID or invalid game!", gameElement)
-		}
-	});
-	document.addEventListener("keydown", (event) => {
-		if ([...document.querySelectorAll("dialog, button")].some(elem => elem.contains(event.target))) {
-			return;
-		}
-		if (event.key === "ArrowLeft") {
-			prevGame();
-		} else if (event.key === "ArrowRight") {
-			nextGame();
-		} else if (["Enter", " "].includes(event.key)) {
-			startGame(getCurrentGame());
-		}
-	});
-	const mc = new Hammer(window, {});
-	mc.on("swipeleft swiperight", e => {
-		switch (e.type) {
-			case "swiperight":
-				prevGame();
-				break;
-			case "swipeleft":
-				nextGame();
-				break;
-		}
-	});
+  gameTemplate = document.querySelector("#game-template");
+  gamesElement = document.querySelector("#games");
+  renderGames();
+  gamesElement.addEventListener("click", (event) => {
+    let gameElement = event.target;
+    while (gameElement && !gameElement.classList.contains("game")) {
+      gameElement = gameElement.parentElement;
+    }
+    if (gameElement && gameElement.dataset.gameId) {
+      const gameID = gameElement.dataset.gameId;
+      if (gameElement.id === "current-game") {
+        startGame(gameID);
+      } else {
+        switchGame(gameID);
+      }
+    } else {
+      console.warn("No game ID or invalid game!", gameElement);
+    }
+  });
+  document.addEventListener("keydown", (event) => {
+    if (
+      [...document.querySelectorAll("dialog, button")].some((elem) =>
+        elem.contains(event.target),
+      )
+    ) {
+      return;
+    }
+    if (event.key === "ArrowLeft") {
+      prevGame();
+    } else if (event.key === "ArrowRight") {
+      nextGame();
+    } else if (["Enter", " "].includes(event.key)) {
+      startGame(getCurrentGame());
+    }
+  });
+  const mc = new Hammer(window, {});
+  mc.on("swipeleft swiperight", (e) => {
+    switch (e.type) {
+      case "swiperight":
+        prevGame();
+        break;
+      case "swipeleft":
+        nextGame();
+        break;
+    }
+  });
 });
